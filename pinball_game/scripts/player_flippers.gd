809b7798a4ba1027106068362ae6f12a09cea733
rielvis <rielvis.xyz@gmail.com>
2026-03-05 14:05:39 +0000

    Added input checking for flippers

--- pinball_game/scripts/player_flippers.gd
+++ pinball_game/scripts/player_flippers.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var leftFlipperFlipped : bool = false
+var rightFlipperFlipped : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _unhandled_input(event: InputEvent) -> void:
+	# Checking for left flipper player input
+	if event.is_action_pressed("A") || event.is_action_pressed("leftArrow"):
+		leftFlipperFlipped = true
+	elif event.is_action_released("A") || event.is_action_released("leftArrow"):
+		leftFlipperFlipped = false
+	
+	# Checking for right flipper player input
+	if event.is_action_pressed("D") || event.is_action_pressed("rightArrow"):
+		leftFlipperFlipped = true
+	elif event.is_action_released("D") || event.is_action_released("rightArrow"):
+		leftFlipperFlipped = false
